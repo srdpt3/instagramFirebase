476a63fdd0e9503d0494baa372317b33444af9c1
Brian Voong <bhlvoong@gmail.com>
2017-03-18 20:16:04 -0700

    Ep 2 UIColor and Anchor Extension

---
+++ InstagramFirebase/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Brian Voong on 3/18/17.
+//  Copyright © 2017 Lets Build That App. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+//    func someRandomeMethod() {
+//        
+//    }
+    
+}
